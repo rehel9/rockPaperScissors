8dbd6d26b35e2d865843fa7b63fc580c567bf869
Luis Leonardo <llrwork1@gmail.com>
2022-06-23 09:03:13 -0500

    Change variable type of computerChoice in computerPlay function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function computerPlay () { // This function creates a random number, converts it into an int, and returns a string depending on the result
-    const computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
         return (computerChoice === 0) ? "scissors"
               :(computerChoice === 1) ? "rock"
               :(computerChoice === 2) ? "paper"
