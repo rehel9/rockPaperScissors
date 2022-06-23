e10a6a1f004dd2d303d3680a02f0303a146d5000
Luis Leonardo <llrwork1@gmail.com>
2022-06-23 09:33:56 -0500

    Add playRound function to index.js and test it

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ function computerPlay () { // This function creates a random number, converts it
     let computerChoice = Math.floor(Math.random() * 3);
         return (computerChoice === 0) ? "scissors"
               :(computerChoice === 1) ? "rock"
-              :(computerChoice === 2) ? "paper"
-              :"scissors";
+              :"paper"
 }
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){//Draw condition
+        return `Draw! ${playerSelection} is the same as ${computerSelection}` ;
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors"){ //win condition
+        return "You win! Rock beats Scissors!" ;
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return "You win! Scissors beats Paper!" ;
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You win! Paper beats Rock!" ;
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock"){ //lose condition
+        return "You lose! Rock beats Scissors!" ;
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        return "You lose! Scissors beats Paper!" ;
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        return "You lose! Paper beats Rock!" ;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
