0df295276166c56e8ac9c04772034a2ce76d89b3
Luis Leonardo <llrwork1@gmail.com>
2022-06-23 09:01:26 -0500

    Create computerPlay function in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function computerPlay () { // This function creates a random number, converts it into an int, and returns a string depending on the result
+    const computerChoice = Math.floor(Math.random() * 3);
+        return (computerChoice === 0) ? "scissors"
+              :(computerChoice === 1) ? "rock"
+              :(computerChoice === 2) ? "paper"
+              :"scissors";
+}
